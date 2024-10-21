a91a0c1731cee63dd6d17f63d4faeff7e785b8cf
Jakub Miarka <jakvbmiarka@gmail.com>
2024-10-21 17:53:40 +0200

    update on search page

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -2,101 +2,125 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class SearchPage extends StatefulWidget {
-    @override
+  @override
   State<StatefulWidget> createState() {
     return SearchState();
   }
-
-
 }
 
-class SearchState extends State<SearchPage>
-{
+class SearchState extends State<SearchPage> {
   List<int> cookingTimes = [];
-  List<String> allIngredients = ['brokul', 'kalafior']; //tutaj bym wczytal potem wszystkie skladniki i zrobil wyszukiwarke nie
+  List<String> allIngredients = [
+    'brokul',
+    'kalafior'
+  ]; //tutaj bym wczytal potem wszystkie skladniki i zrobil wyszukiwarke nie
   List<String> selectedIngredients = [];
   int? _chosenTime;
   @override
   void initState() {
-    for(int i=0; i<20; i++)
-    {
-      cookingTimes.add((i+1) * 5);
+    for (int i = 0; i < 20; i++) {
+      cookingTimes.add((i + 1) * 5);
     }
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold
-    (
-      appBar: AppBar(title: Text("Wyszukaj danie", style: TextStyle(fontSize: 48, fontWeight: FontWeight.w500)), centerTitle: true,),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          timeSelection(),
-          SizedBox(height: 20),
-          ingredientsSelection()
-        ],
-      )
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Wyszukaj danie",
+              style: TextStyle(fontSize: 48, fontWeight: FontWeight.w500)),
+          centerTitle: true,
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            timeSelection(),
+            SizedBox(height: 20),
+            ingredientsSelection()
+          ],
+        ));
+  }
+
+  Padding timeSelection() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+          height: 100,
+          decoration: BoxDecoration(
+              border: Border.all(width: 8, color: Colors.black),
+              borderRadius: BorderRadius.circular(16)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Wybierz preferowany czas przygotowania potrawy',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+              ),
+              DropdownButton<int>(
+                  hint: Text("Pick"),
+                  value: _chosenTime,
+                  items: cookingTimes.map((int value) {
+                    return new DropdownMenuItem<int>(
+                      value: value,
+                      child: new Text(value.toString()),
+                    );
+                  }).toList(),
+                  onChanged: (newVal) {
+                    setState(() {
+                      _chosenTime = newVal!;
+                    });
+                  })
+            ],
+          )),
     );
   }
 
-  Padding timeSelection()
-  {
+  Padding ingredientsSelection() {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container
-      (
-        height: 100,
-        decoration: BoxDecoration(border: Border.all(width: 8, color: Colors.black), borderRadius: BorderRadius.circular(16)),
-        child:Row
-        (
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: 
-          [
-            Text('Wybierz preferowany czas przygotowania potrawy', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),),
-            DropdownButton<int>(
-        hint: Text("Pick"),
-        value: _chosenTime,
-        items: cookingTimes.map((int value) {
-          return new DropdownMenuItem<int>(
-            value: value,
-            child: new Text(value.toString()),
+      padding: const EdgeInsets.all(8),
+      child: Container(
+          height: 300,
+          decoration: BoxDecoration(
+              border: Border.all(width: 8, color: Colors.black),
+              borderRadius: BorderRadius.circular(16)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextField(
+                  decoration: InputDecoration(hintText: 'Wyszukaj składniki')),
+              Container(child: displayIngredients(''), height: 120,)
+            ],
+          )),
+    );
+  }
+
+  ListView displayIngredients(String pattern) {
+    List<String> ingredients = [];
+    if (pattern.isNotEmpty) {
+      for (int i = 0; i < allIngredients.length; i++) {
+        if (allIngredients[i].contains(pattern)) {
+          ingredients.add(allIngredients[i]);
+        }
+      }
+    } else
+      {ingredients = allIngredients;}
+    return ListView.separated(
+        padding: const EdgeInsets.all(8),
+        separatorBuilder: (BuildContext context, int Index) => SizedBox(width: 10,),
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            decoration: BoxDecoration(
+                          border: Border.all(width: 2, color: Colors.black),
+                          borderRadius: BorderRadius.circular(12)),
+            height: 80,
+            width: 160,
+            child: Text(ingredients[index], textAlign: TextAlign.center,),
           );
-        }).toList(),
-        onChanged: (newVal) {
-          setState(() {
-            _chosenTime = newVal!;
-          });
-        })
-
-          ],
-        )
-      ),
-    );
-  } 
-  
-
-  Padding ingredientsSelection()
-  {
-    return Padding(padding: const EdgeInsets.all
-    (8),
-    child:
-     Container
-     (
-      height: 200,
-      decoration: BoxDecoration(border: Border.all(width: 8, color: Colors.black), borderRadius: BorderRadius.circular(16)),
-      child: Column
-      (
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: 
-        [
-          TextField(decoration: InputDecoration(hintText: 'Wyszukaj składniki'))
-        ],
-      )      
-     ),
-    );
+        },
+        itemCount: ingredients.length);
   }
 }
