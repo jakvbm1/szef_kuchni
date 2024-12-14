5865afc67ec640b95b2cd8828c95ff131360b258
dk5042 <damianknopek03@gmail.com>
2024-12-14 15:20:02 +0100

    fixed some more issues with the search

--- lib/views/all_recipes_view.dart
+++ lib/views/all_recipes_view.dart
@@ -4,7 +4,7 @@ import 'package:szef_kuchni_v2/models/recipe_model.dart';
 import 'package:szef_kuchni_v2/services/database_service.dart';
 import 'package:szef_kuchni_v2/views/recipe_view.dart';
 
-class AllRecipesView extends StatefulWidget{
+class AllRecipesView extends StatefulWidget {
   const AllRecipesView({super.key});
 
   @override
@@ -15,7 +15,7 @@ class _AllRecipesViewState extends State<AllRecipesView> {
   //=====================================================
   //================== VARIABLES ========================
   //=====================================================
-  
+
   // Use this to import databaseService functionality
   final DatabaseService databaseService = DatabaseService();
   // Sets a trigger that is used to load more recipes with pagination
@@ -32,6 +32,8 @@ class _AllRecipesViewState extends State<AllRecipesView> {
   bool _hasMore = true;
   int _batchNumber = 0;
   static const int _batchSize = 40;
+
+  // Dynamic search delay
   bool _isSearching = false;
   Timer? _debounce;
 
@@ -43,6 +45,9 @@ class _AllRecipesViewState extends State<AllRecipesView> {
   int _maxTime = 0;
   final List<String> _selectedIngredients = [];
   final List<String> _selectedCategories = [];
+
+  // Globar string with user search input
+  String userSearchInput = '';
 
   //=====================================================
   //================== FUNCTIONS ========================
@@ -77,7 +82,7 @@ class _AllRecipesViewState extends State<AllRecipesView> {
 
   // Called when the _scrollController trigger is activated
   void _onScroll() {
-    if (_scrollController.position.pixels >= 
+    if (_scrollController.position.pixels >=
         _scrollController.position.maxScrollExtent * 0.8) {
       _loadMoreRecipes();
     }
@@ -97,7 +102,7 @@ class _AllRecipesViewState extends State<AllRecipesView> {
       selectedIngredients: _selectedIngredients,
       selectedCategories: _selectedCategories,
     );
-    
+
     setState(() {
       if (newRecipes.isEmpty) {
         _hasMore = false;
@@ -115,6 +120,7 @@ class _AllRecipesViewState extends State<AllRecipesView> {
   Future<void> _displaySearchResults(String enteredKeyword) async {
     setState(() {
       _isLoading = true;
+      _isSearching = true;
       _recipes.clear(); // Clear existing recipes
     });
 
@@ -127,7 +133,7 @@ class _AllRecipesViewState extends State<AllRecipesView> {
       selectedCategories: _selectedCategories,
       enteredKeyword: enteredKeyword,
     );
-    
+
     setState(() {
       _recipes = newRecipes;
       _isLoading = false;
@@ -160,7 +166,7 @@ class _AllRecipesViewState extends State<AllRecipesView> {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
-    
+
     // LayoutBuilder is used to get the height of the view
     // height is used to determine the height of the filter panel
     // i dont like this and i want to change it later
@@ -172,47 +178,51 @@ class _AllRecipesViewState extends State<AllRecipesView> {
           // MAIN COLUMN
           body: Column(
             children: <Widget>[
-
               searchBar(),
-
-              filtersExpandablePanel(theme, viewHeight, constraints),
-
-              _isSearching
-              ? loadingDisplay(theme)
-              : recipesDisplay(theme)
-            ]
+              filtersExpandablePanel(
+                theme,
+                viewHeight,
+                constraints,
+              ),
+              _isSearching ? loadingDisplay(theme) : recipesDisplay(theme)
+            ],
           ),
         );
-      }
+      },
     );
   }
 
   // MAIN SEARCH BAR
   Padding searchBar() {
     return Padding(
-              padding: const EdgeInsets.only(top: 8, bottom: 8),
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    onChanged:(value) => _displaySearchResultsWithDelay(value),
-                    decoration: const InputDecoration(
-                      label: Text(
-                        "Wyszukaj po nazwie",
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                      suffixIcon: Icon(Icons.search),
-                    ),
-                  ),
+      padding: const EdgeInsets.only(top: 8, bottom: 8),
+      child: Card(
+        child: Padding(
+          padding:
+              const EdgeInsets.only(left: 16, right: 16, bottom: 16, top: 8),
+          child: TextField(
+            onChanged: (value) {
+              userSearchInput = value;
+              _displaySearchResultsWithDelay(userSearchInput);
+            },
+            decoration: const InputDecoration(
+              label: Text(
+                "Wyszukaj po nazwie",
+                style: TextStyle(
+                  fontSize: 20,
                 ),
               ),
-            );
+              suffixIcon: Icon(Icons.search),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   // FILTERS PANEL
-  ExpansionPanelList filtersExpandablePanel(ThemeData theme, double viewHeight, BoxConstraints constraints) {
+  ExpansionPanelList filtersExpandablePanel(
+      ThemeData theme, double viewHeight, BoxConstraints constraints) {
     return ExpansionPanelList(
       children: [
         ExpansionPanel(
@@ -232,14 +242,13 @@ class _AllRecipesViewState extends State<AllRecipesView> {
 
           // BODY OF FILTER SETTINGS PANEL
           body: SizedBox(
-            height: viewHeight * 0.75, // it needs to be a fixed height, code breaks otherwise
+            height: viewHeight *
+                0.74, // it needs to be a fixed height, code breaks otherwise
             child: SingleChildScrollView(
-
               // MAIN FILTER SETTINGS COLUMN
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-
                   // TIME SETTING OBJECT
                   Container(
                     width: constraints.maxWidth,
@@ -249,7 +258,7 @@ class _AllRecipesViewState extends State<AllRecipesView> {
                       border: Border(
                         top: BorderSide(
                           color: theme.colorScheme.onSurface,
-                        )
+                        ),
                       ),
                     ),
                     child: Row(
@@ -266,7 +275,7 @@ class _AllRecipesViewState extends State<AllRecipesView> {
                               keyboardType: TextInputType.number,
                               onChanged: (value) {
                                 if (value.isEmpty) {
-                                  _minTime = 0; 
+                                  _minTime = 0;
                                 } else {
                                   _minTime = int.parse(value);
                                 }
@@ -292,7 +301,8 @@ class _AllRecipesViewState extends State<AllRecipesView> {
                               keyboardType: TextInputType.number,
                               onChanged: (value) {
                                 if (value.isEmpty) {
-                                  _maxTime = 0; // or any default value you prefer
+                                  _maxTime =
+                                      0; // or any default value you prefer
                                 } else {
                                   _maxTime = int.parse(value);
                                 }
@@ -323,12 +333,17 @@ class _AllRecipesViewState extends State<AllRecipesView> {
                       border: Border.symmetric(
                         horizontal: BorderSide(
                           color: theme.colorScheme.onSurface,
-                        )
+                        ),
                       ),
                     ),
-                    child: filterComponentWithSearchBar(theme, "Składniki", "wyszukaj składnik", _ingredientsNames, _selectedIngredients),
+                    child: filterComponentWithSearchBar(
+                        theme,
+                        "Składniki",
+                        "wyszukaj składnik",
+                        _ingredientsNames,
+                        _selectedIngredients),
                   ),
-                  
+
                   // CATEGORIES SETTING OBJECT
                   Container(
                     width: constraints.maxWidth,
@@ -338,28 +353,34 @@ class _AllRecipesViewState extends State<AllRecipesView> {
                       border: Border(
                         bottom: BorderSide(
                           color: theme.colorScheme.onSurface,
-                        )
-                      )
+                        ),
+                      ),
                     ),
-                    child: filterComponentWithSearchBar(theme, "Kategorie", "wyszukaj kategorie", _categoriesNames, _selectedCategories),
+                    child: filterComponentWithSearchBar(
+                        theme,
+                        "Kategorie",
+                        "wyszukaj kategorie",
+                        _categoriesNames,
+                        _selectedCategories),
                   ),
 
                   // APPLY FILTERS BUTTON
                   ElevatedButton(
-                    onPressed: (){
+                    onPressed: () {
                       setState(() {
                         _recipes.clear();
                         _batchNumber = 0;
                         _loadMoreRecipes();
+                        _displaySearchResults(userSearchInput);
                         expansionPanelState = false;
                       });
-                    }, 
+                    },
                     child: const Text(
                       "Zastosuj filtry",
                       style: TextStyle(
                         fontSize: 20,
                       ),
-                    )
+                    ),
                   ),
                 ],
               ),
@@ -374,11 +395,16 @@ class _AllRecipesViewState extends State<AllRecipesView> {
       },
     );
   }
-  
+
   // FILTERS PANEL COMPONENT - FILTER COMPONENT WITH SEARCH BAR
   // Ingredients filter component and category filter component were basically the same
   // so i made a function that efficiently creates them with only few given parameters
-  Column filterComponentWithSearchBar(ThemeData theme, String labelName, String hintLabelName, List<String> autocompeteDataList, List<String> selectedDataList) {
+  Column filterComponentWithSearchBar(
+      ThemeData theme,
+      String labelName,
+      String hintLabelName,
+      List<String> autocompeteDataList,
+      List<String> selectedDataList) {
     late TextEditingController textEditingController;
 
     return Column(
@@ -395,23 +421,26 @@ class _AllRecipesViewState extends State<AllRecipesView> {
                     if (textEditingValue.text.isEmpty) {
                       return const Iterable<String>.empty();
                     }
-                    return autocompeteDataList.where((object) => object.contains(textEditingValue.text));
+                    return autocompeteDataList.where(
+                      (object) => object.contains(textEditingValue.text),
+                    );
                   },
                   onSelected: (String selectedResult) {
-                    setState(() {
-                      if (selectedDataList.contains(selectedResult)) {
+                    setState(
+                      () {
+                        if (selectedDataList.contains(selectedResult)) {
+                          textEditingController.text = "";
+                          return;
+                        }
+                        selectedDataList.add(selectedResult);
                         textEditingController.text = "";
-                        return;
-                      }
-                      selectedDataList.add(selectedResult);
-                      textEditingController.text = "";
-                    });
+                      },
+                    );
                   },
-                  fieldViewBuilder: (
-                  BuildContext context, 
-                  TextEditingController fieldTextEditingController,
-                  FocusNode fieldFocusNode, 
-                  VoidCallback onFieldSubmitted) {
+                  fieldViewBuilder: (BuildContext context,
+                      TextEditingController fieldTextEditingController,
+                      FocusNode fieldFocusNode,
+                      VoidCallback onFieldSubmitted) {
                     textEditingController = fieldTextEditingController;
                     return TextField(
                       controller: fieldTextEditingController,
@@ -439,33 +468,40 @@ class _AllRecipesViewState extends State<AllRecipesView> {
           alignment: WrapAlignment.center,
           spacing: 8,
           runSpacing: 8,
-          children: selectedDataList.map((object) => Card(
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-                  child: Text(
-                    object,
-                    textAlign: TextAlign.center,
-                  ),
+          children: selectedDataList
+            .map(
+              (object) => Card(
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 12, vertical: 8),
+                      child: Text(
+                        object,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(
+                          () {
+                            selectedDataList.remove(object);
+                            if (labelName == "Składniki") {
+                              _selectedIngredients.remove(object);
+                            } else if (labelName == "Kategorie") {
+                              _selectedCategories.remove(object);
+                            }
+                          },
+                        );
+                      },
+                      child: const Icon(Icons.close),
+                    ),
+                  ],
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      selectedDataList.remove(object);
-                      if (labelName == "Składniki") {
-                        _selectedIngredients.remove(object);
-                      } else if (labelName == "Kategorie") {
-                        _selectedCategories.remove(object);
-                      }
-                    });
-                  },
-                  child: const Icon(Icons.close),
-                ),
-              ],
-            ),
-          )).toList(),
+              ),
+            )
+            .toList(),
         ),
       ],
     );
@@ -478,28 +514,26 @@ class _AllRecipesViewState extends State<AllRecipesView> {
         padding: const EdgeInsets.all(5.0),
         child: Card(
           color: theme.colorScheme.surfaceContainerHigh,
-          child: 
-            _recipes.isNotEmpty
+          child: _recipes.isNotEmpty
             ? ListView.builder(
                 controller: _scrollController,
                 itemCount: _recipes.length + (_hasMore ? 1 : 0),
                 itemBuilder: (context, index) {
-
                   // Displays loading indicator when loading from database is not finished
                   // im not sure if it works correctly
                   if (index >= _recipes.length) {
                     return const Center(
                       child: Padding(
-                        padding: EdgeInsets.all(16),
+                        padding: EdgeInsets.all(16.0),
                         child: CircularProgressIndicator(),
                       ),
-                    ); 
+                    );
                   }
                   // Displays recipe tiles
                   // this need to be changed to a recipe button that opens a recipe view
                   return GestureDetector(
                     child: Padding(
-                      padding: const EdgeInsets.all(15.0),
+                      padding: const EdgeInsets.only(top: 16.0, right: 16.0, left: 16.0),
                       child: Card(
                         color: theme.colorScheme.surface,
                         child: Padding(
@@ -508,13 +542,22 @@ class _AllRecipesViewState extends State<AllRecipesView> {
                         ),
                       ),
                     ),
-                    onTap: (){Navigator.push(context, MaterialPageRoute(builder: (context) => RecipeView(recipe: _recipes[index])));},
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => RecipeView(
+                            recipe: _recipes[index],
+                          ),
+                        ),
+                      );
+                    },
                   );
                 },
               )
             : const Center(
-                child: Text("No results"),
-              )
+                child: Text("Brak wyników"),
+              ),
         ),
       ),
     );
@@ -524,16 +567,17 @@ class _AllRecipesViewState extends State<AllRecipesView> {
   Column recipeTile(int index, ThemeData theme) {
     return Column(
       children: [
-       // Text(_recipes[index].id.toString(),),
-        Text(_recipes[index].name,),
-        //Text(_recipes[index].minutes.toString(),),
-        //Text(_recipes[index].nutrition,),
-        //Text(_recipes[index].steps,),
-        Text(_recipes[index].isFavourite.toString(),),
+        // Text(_recipes[index].id.toString(),),
+        Text(_recipes[index].name),
+        //Text(_recipes[index].minutes.toString()),
+        //Text(_recipes[index].nutrition),
+        //Text(_recipes[index].steps),
+        Text(_recipes[index].isFavourite.toString()),
       ],
     );
   }
 
+  // Card display with a loading circle
   Expanded loadingDisplay(ThemeData theme) {
     return Expanded(
       child: Padding(
